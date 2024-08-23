58a67d003230d3c4a5954e382e21c2d8132cb3aa
LunaFreya Dragonborn <rose.sky000801@gmail.com>
2024-08-23 10:32:22 +0200

    added date and timezone libraries. added function to display date and time, updating every second.

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,36 @@
-//js for updating date and time for oslo
+function updateTime() {
+//js for updating local date and time
+let localElement = document.querySelector("#local");
+let localDateElement = localElement.querySelector(".date");
+let localTimeElement = localElement.querySelector(".time");
 
-let osloElement = document.querySelector("#oslo");
-let osloDateElement = osloElement.querySelector(".date");
-let osloTimeElement = osloElement.querySelector(".time");
-let osloTime = moment();
+localDateElement.innerHTML = moment().format("ddd, Do MMMM, YYYY");
+localTimeElement.innerHTML = moment().format("h:mm:ss [<small>]A[</small>]");
 
-osloDateElement.innerHTML = moment().format("ddd, Do MMMM, YYYY");
-osloTimeElement.innerHTML = `${osloTime.tz("Europe/Oslo").format("h:mm:ss")} <small>${osloTime.format("A")}</small>`;
+//js for updating date and time for paris
+let parisElement = document.querySelector("#paris");
+let parisDateElement = parisElement.querySelector(".date");
+let parisTimeElement = parisElement.querySelector(".time");
 
+parisDateElement.innerHTML = moment().format("ddd, Do MMMM, YYYY");
+parisTimeElement.innerHTML = moment().tz("Europe/Paris").format("h:mm:ss [<small>]A[</small>]");
 
+//js for updating date and time for New York
+let newYorkElement = document.querySelector("#new-york");
+let newYorkDateElement = newYorkElement.querySelector(".date");
+let newYorkTimeElement = newYorkElement.querySelector(".time");
 
+newYorkDateElement.innerHTML = moment().format("ddd, Do MMMM, YYYY");
+newYorkTimeElement.innerHTML = moment().tz("America/New_York").format("h:mm:ss [<small>]A[</small>]");
+
+//js for updating date and time for Beijing
+let beijingElement = document.querySelector("#beijing");
+let beijingDateElement = beijingElement.querySelector(".date");
+let beijingTimeElement = beijingElement.querySelector(".time");
+
+beijingDateElement.innerHTML = moment().format("ddd, Do MMMM, YYYY");
+beijingTimeElement.innerHTML = moment().tz("Asia/Manila").format("h:mm:ss [<small>]A[</small>]");
+};
+
+updateTime();
+setInterval(updateTime, 1000);
